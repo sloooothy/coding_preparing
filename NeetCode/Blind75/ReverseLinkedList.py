e93643f3e4c0a852719a5b053ed5683c35c82dc8
sloooothy <116268191+sloooothy@users.noreply.github.com>
2025-07-14 16:33:06 +0800

    Create ReverseLinkedList.py

---
+++ NeetCode/Blind75/ReverseLinkedList.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev,cur=None, head
+
+        while(cur):
+            cur.next, prev, cur=prev,cur,cur.next #python exchange
+
+        return prev
