48f95a234fb70f7aba939dcc60991493a3d01954
sloooothy <116268191+sloooothy@users.noreply.github.com>
2025-07-23 10:38:09 +0800

    Delete Leetcode/Grind 75 INTERVIEW PRACTICE/twoSum.py
    delete python solution

--- Leetcode/Grind 75 INTERVIEW PRACTICE/twoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashset={} # set to keep nums's element and position
-        for i,n in enumerate(nums):
-            diff=target-n # the remain value
-            if diff in hashset: # if the remain value was lay in previous position
-                return [hashset[diff],i] # return the previous position and current position
-                
-            hashset[n]=i #if not, keep the position record for current number
