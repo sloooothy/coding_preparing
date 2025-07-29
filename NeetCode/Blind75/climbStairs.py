cb657983c2048f3d77eab0680b3edda5cb7d1d6b
sloooothy <116268191+sloooothy@users.noreply.github.com>
2025-07-29 17:43:38 +0800

    Climbing Stairs

---
+++ NeetCode/Blind75/climbStairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    """concept: Fibonacci sequence""" 
+    def climbStairs(self, n: int) -> int:
+        # base cases
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        # keep previous step methods
+        steps=[0]*n
+        steps[0]=1
+        steps[1]=2
+        for i in range(2,n):
+            steps[i]=steps[i-1]+steps[i-2]
+
+        return steps[n-1]
