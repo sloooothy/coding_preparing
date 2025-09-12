17c34fc0e818bf49a71aea20d9406e7025ec4742
sloooothy <116268191+sloooothy@users.noreply.github.com>
2025-09-12 16:16:25 +0800

    Maximum Subarray Solved , DP, Kadane's Algorithm

---
+++ NeetCode/Blind75/maxSubArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        if len(nums)==1:
+            return nums[0]
+
+        
+        for i in range(1,len(nums)):
+            nums[i]=max(nums[i],nums[i-1]+nums[i])
+            
+
+        return max(nums)
+            
