190c8f55e24742a133189adcb5c5bbcc42e3e42e
sloooothy <116268191+sloooothy@users.noreply.github.com>
2025-09-12 13:22:03 +0800

    fix error

--- NeetCode/Blind75/decodingway.py
+++ NeetCode/Blind75/decodingway.py
@@ -6,7 +6,9 @@ class Solution:
         def dfs(i):
             if i in dp:
                 return dp[i]
-            if s[0]=='0':
+
+            # 錯誤處理: 如果當前字元為 '0'，無法獨立解碼，所以解碼方式為 0。
+            if s[i] == "0":
                 return 0
 
             res=dfs(i+1)
@@ -16,6 +18,10 @@ class Solution:
                )):
                res+=dfs(i+2)
 
+            # 儲存結果並回傳
+            #    將當前子問題的結果存入 dp，以供未來使用
+            dp[i] = res
             return res
 
-        return dfs(0)
+        return dfs(0) # start from character position = 0 (first char)
+            
